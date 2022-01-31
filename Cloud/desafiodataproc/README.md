922d1511608a7c6d25fcc11e545f2787fc6a0141
Cintia Belem <cintiambelem@gmail.com>
2022-01-31 09:37:57 -0300

    commit inicial

---
+++ Cloud/desafiodataproc/README.md
@@ -0,0 +1,10 @@
+# Desafio Digital Innovation One - DIO
+
+Desafio desenvolvido com base no treinamento fornecido por Marcelo Marques na <a href="https://web.dio.me/lab/criando-um-ecossistema-hadoop-totalmente-gerenciado-com-google-cloud-dataproc/learning/f2a19382-6bbf-4620-a94d-5bb1a46c00c5"> Digital Innovation One</a>. 
+
+O lab consiste em desenvolver um ecossistema Hadoop totalmente gerenciado pelo Google Cloud Platform (GCP) utilizando o produto Dataproc. O sistema realizou a contagem das palavras de um livro, e retornou o resultado.
+
+Parte importante do treinamento é a explicação soobre os produtos Google, máquinas virtuais (virtual machine/compute engine), Dataproc e outros.
+
+
+<a href="https://github.com/marcelomarques05/dio-desafio-dataproc"> Repositório DIO do desafio</a>.
---
+++ Spring/saladereuniao/backend/src/main/java/com/digital/crud/saladereuniao/saladereuniao/controller/RoomController.java
@@ -0,0 +1,68 @@
+package com.digital.crud.saladereuniao.saladereuniao.controller;
+
+
+import com.digital.crud.saladereuniao.saladereuniao.exception.ResourcesNotFoundException;
+import com.digital.crud.saladereuniao.saladereuniao.model.Room;
+import com.digital.crud.saladereuniao.saladereuniao.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController @CrossOrigin(origins = "http://localhost:4200") //porta padrão angular.
+@RequestMapping("/api/v1")
+public class RoomController {
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @GetMapping("/rooms")
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @GetMapping("/rooms/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable(value = "id") long roomId)
+            throws ResourcesNotFoundException {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Room not found :: " + roomId));
+        return ResponseEntity.ok().body(room);
+    }
+
+    @PostMapping("/rooms")
+    public Room createRoom(@Valid @RequestBody Room room) {
+        return roomRepository.save(room);
+    }
+
+    @PutMapping("/rooms/{id}")
+    public ResponseEntity < Room > updateRoom(@PathVariable(value = "id") Long roomId, @Valid @RequestBody Room roomDetails) throws ResourcesNotFoundException {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Room not found for this id :: " + roomId));
+        room.setName(roomDetails.getName());
+        room.setDate(roomDetails.getDate());
+        room.setStartHour(room.getStartHour());
+        room.setEndHour(roomDetails.getEndHour());
+        final Room updateRoom = roomRepository.save(room);
+        return ResponseEntity.ok(updateRoom);
+
+    }
+
+    @DeleteMapping("/rooms/{id}")
+    public Map<String, Boolean> deleteRoom(@PathVariable(value = "id") Long roomId)
+            throws ResourcesNotFoundException {
+        Room room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new ResourcesNotFoundException("Room not found for this id :: " + roomId));
+
+        roomRepository.delete(room);
+        Map<String, Boolean> response = new HashMap< >();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+}
+
+
+
